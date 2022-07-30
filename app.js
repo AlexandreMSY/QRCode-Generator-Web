3c416b92488bec7ce962a4b84e993db01b1bb38b
Alexandre Massao <alexandremsubs@gmail.com>
2022-07-30 19:09:48 -0300

    Working size adjustment

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ var qrcode = undefined;
 
 function generateQR(value){
     let qrHolder = document.getElementById('qrcode');
-
+    
     let options = {
         text: value,
+        width: parseInt(document.getElementById('width').value),
+        height: parseInt(document.getElementById('height').value)
     };
 
+    console.log(typeof options.width)
+
+
     if (qrcode === undefined){
-        qrcode = new QRCode(qrHolder,options);
+        qrcode = new QRCode(qrHolder, options);
     }else{
-        qrcode.makeCode(value)
+        qrcode.clear() /*in case a qr code already exists, it just creates another one*/
+        qrcode = new QRCode(qrHolder, options);
     }
 }
